0e93ead2998d20ee3739ffe1c4bf09246c9e3b49
RATCHANON CHUNSAKUNEE <kirigayannd6355@gmail.com>
2023-09-14 15:16:42 +0700

    message_maybe

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -6,6 +6,41 @@ import { NextResponse } from "next/server";
 export const GET = async (request) => {
   readDB();
 
+  let roomId = null;
+
+  let role = null;
+
+  try{
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    roomId = payload.roomID;
+    role = payload.role;
+  }catch{
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Room is not found",
+      },
+      {status: 404}
+    );
+  }
+
+  if (role === "ADMIN" || "SUPER_ADMIN"){
+    return NextResponse.json({
+      ok: ture,
+      enrollments: DB.enrollments,
+    });
+  }else{
+    const courseNoList = [];
+    for(const enroll of DB.enrollments){
+      if(enroll.roomId === roomId){
+        courseNoList.push(enroll.courseNo);
+      }
+    }
+    return NextResponse.json({
+      ok: true,
+      courseNoList,
+    });
+  }
   // return NextResponse.json(
   //   {
   //     ok: false,
@@ -18,6 +53,68 @@ export const GET = async (request) => {
 export const POST = async (request) => {
   readDB();
 
+  const rawAuthHeader = headers().get("authorization");
+  const token = rawAuthHeader.split(" ")[1];
+  let roomId = null;
+
+  let role = null;
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    roomId = payload.studentId;
+    role = payload.role;
+  } catch {
+    return NextResponse.json(
+    {
+      ok: false,
+      messag: "Invalid token",
+    },
+    { status 401 }
+    );
+  }
+
+  if (role === "ADMIN" || "SUPER_ADMIN"){
+    return NextResponse.json(
+      {
+        ok: ture,
+        message: "Spoil One Piece",
+      },
+      { status: 403 } 
+    );
+  }else{
+    const body = await request.json();
+    const {courseNo} = body;
+    if(typeof courseNo !== "string" || courseNo.length !== 6){
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "Room Spoil One Piece already exists",
+        },
+        {status: 400}
+      );
+    }
+
+    const foundCourse = DB.courses.find((x) => x.courseNo === courseNo);
+    if(!foundCourse){
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "Room Spoil One Piece already exists",
+        };
+        {status: 400}
+      );
+    }
+
+    DB.enrollments.push({
+      roomId,
+      courseNo,
+    });
+
+    return NextResponse.json({
+      ok: ture,
+      message: "You has enrolled a course successfully",
+    });
+  }
   // return NextResponse.json(
   //   {
   //     ok: false,
@@ -40,6 +137,66 @@ export const POST = async (request) => {
 export const DELETE = async (request) => {
   const payload = checkToken();
 
+  const rawAuthHeader = headers().get("authorization");
+  const token = rawAuthHeader.split(" ")[1];
+  let roomID = null;
+  let role = null;
+
+  try{
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    roomID = payload.roomId;
+    role = payload.role;
+  }catch{
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      {status: 401}
+    );
+  }
+
+  if (role === "ADMIN" || "SUPER_ADMIN"){
+    return NextResponse.json(
+      {
+        ok: ture,
+        message: "message has been deleted",
+      },
+      {status: 403}
+    );
+  } else {
+    const body = await request.json();
+    const { courseNo}=body;
+    if(typeof courseNo !== "string" || courseNo.length !== 6){
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "Invalid token",
+        },
+        {status: 400}
+      );
+    }
+
+    const foundIndex = DB.enrollments.findIndex(
+      (x) => x.roomId === roomId && x.courseNo === courseNo
+    );
+    if(foundIndex === -1){
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "message is not found",
+        },
+        {status: 404}
+      );
+    }
+
+    DB.enrollments.splice(foundIndex, 1);
+
+    return NextResponse.json({
+      ok: ture,
+      message: "You has dropped from this course, See you next semester.",
+    });
+  }
   // return NextResponse.json(
   //   {
   //     ok: false,
